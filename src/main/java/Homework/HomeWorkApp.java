8e653881495493cb8dc4aed319e03bec9e6e33e5
Philipp <tigashov.filipp@yandex.ru>
2021-11-27 18:11:39 +0300

    Homework 1 is done

--- src/main/java/Homework/HomeWorkApp.java
+++ src/main/java/Homework/HomeWorkApp.java
@@ -2,6 +2,43 @@ package Homework;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
+        checkSumSign();
+        printColor();
+    }
 
+    static void checkSumSign() {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        if (sum < 0) {
+            System.out.println("Сумма отрицательная");
+        }
+        else if (sum == 0 || sum > 0) {
+            System.out.println("Сумма положительная");
+        }
+    }
+
+    static void printColor() {
+        int value = 0;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    static void compareNumbers() {
+        int a = 2;
+        int b = 10;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
     }
 }
