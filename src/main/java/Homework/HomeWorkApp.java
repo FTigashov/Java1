c81c26af5ab7a3d71a16f462573123e619f9173e
Philipp <tigashov.filipp@yandex.ru>
2021-11-27 18:13:41 +0300

    Homework 1

--- src/main/java/Homework/HomeWorkApp.java
+++ src/main/java/Homework/HomeWorkApp.java
@@ -4,6 +4,7 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         checkSumSign();
         printColor();
+        compareNumbers();
     }
 
     static void checkSumSign() {
