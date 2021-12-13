9bf13e36d3b1cdfbe25b20e19095672cd0594bc5
Philipp <tigashov.filipp@yandex.ru>
2021-12-13 17:44:59 +0300

    Homework 4

--- src/main/java/Homework/HomeWorkApp.java
+++ src/main/java/Homework/HomeWorkApp.java
@@ -1,7 +1,11 @@
 package Homework;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        TicTacToe.turnGame();
     }
 }
